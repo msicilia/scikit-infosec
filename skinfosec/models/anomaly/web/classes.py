9eb2204a32d6183aacd48eefadc5748291670686
cristianorolim <cristianorolim@gmail.com>
2016-11-28 00:22:36 -0200

    Added two anomaly checks (Web)
    Additional checks:
    * Character frequency in request query
    * Parameter set in request query

--- skinfosec/models/anomaly/web/classes.py
+++ skinfosec/models/anomaly/web/classes.py
@@ -3,6 +3,9 @@ Classes and functions for anomaly detection over webserver log files.
 """
 
 import logging
+import string
+import urlparse
+from scipy.stats import chisquare
 from sklearn.base import BaseEstimator, ClusterMixin
 import pandas as pd
 from apache_log_parser import make_parser
@@ -39,12 +42,13 @@ def preprocess_requests(data, log_format):
     X = pd.DataFrame(tmp, columns=cols)
     return X
 
-class RequestAnomalyDetector(BaseEstimator, ClusterMixin,
-                             BaseAnomalyDetector):
+class RequestAnomalyDetector(BaseEstimator, ClusterMixin, BaseAnomalyDetector):
     """Request anomaly detector.
     Parameters
     ----------
    """
+    all_ascii = string.join([chr(x) for x in range(256)], '')
+
     def __init__(self):
         self.attribute_models_ = {}
         return
@@ -61,6 +65,43 @@ class RequestAnomalyDetector(BaseEstimator, ClusterMixin,
         # Get mean and std
         lengths = X["request_url"].str.len()
         self.attribute_models_["uri_length"] = (lengths.mean(), lengths.std())
+
+        #Verifying character distribution
+        char_freq = []
+        for i in range(256):
+            char_freq.append(0.0)
+        count_non_empty = 0
+        for index, row in X.iterrows():
+            #Ignoring zero length strings / Avoiding division by zero
+            if len(row.request_url) == 0:
+                continue
+            for i in range(256):
+                freq = float(row.request_url.count(self.all_ascii[i])) / len(row.request_url)
+                char_freq[i] += freq
+            count_non_empty += 1
+            if index % 1000 == 0:
+                logging.info('RequestAnomalyDetector - fit - char dist: Processed %d requests', index)
+        for i in range(256):
+            char_freq[i] /= count_non_empty
+        char_freq.sort(reverse=True)
+        #Calculate idealized character distribution (ICD)
+        icd = [char_freq[0]]
+        icd.append(sum(char_freq[1:4]))
+        icd.append(sum(char_freq[4:7]))
+        icd.append(sum(char_freq[7:12]))
+        icd.append(sum(char_freq[12:16]))
+        icd.append(sum(char_freq[16:256]))
+        self.attribute_models_["icd"] = icd
+
+        param_sets = []
+        for index, row in X.iterrows():
+            params = urlparse.parse_qs(urlparse.urlsplit(row.request_url).query)
+            if len(params) > 0:
+                keys_set = set(params.keys())
+                if keys_set not in param_sets:
+                    param_sets.append(set(params.keys()))
+        self.attribute_models_["param_sets"] = param_sets
+
         return self
 
     def predict(self, X):
@@ -71,16 +112,59 @@ class RequestAnomalyDetector(BaseEstimator, ClusterMixin,
         Features are characteristics of the requests.
         New data to check.
         """
-        anomalies = {}
-        anomalous = pd.DataFrame()
+        result = {}
+        #Temporary list to feed the final DataFrame (Performance)
+        tmp_lst = []
         try:
             norm_model = self.attribute_models_["uri_length"]
         except AttributeError:
             logging.warning('RequestAnomalyDetector: call to preditct() without previous fit()')
-            return anomalies
+            return None
+
         for index, row in X.iterrows():
             # TODO: Check more anomaly models
             if len(row.request_url) > norm_model[0] + 2*norm_model[1]:
-                anomalous = anomalous.append(row)
-        anomalies["uri_length"] = anomalous
-        return anomalies
+                tmp_lst.append(True)
+            else:
+                tmp_lst.append(False)
+
+        anomalous = pd.DataFrame(index=X.index, data=tmp_lst, columns=["uri_length"])
+        result["uri_length"] = anomalous.copy()
+
+        tmp_lst = []
+        for index, row in X.iterrows():
+            char_freq = []
+            if len(row.request_url) == 0:
+                tmp_lst.append(0.0)
+                continue
+            for i in range(256):
+                char_count = row.request_url.count(self.all_ascii[i])
+                char_freq.append(char_count)
+            char_freq.sort(reverse=True)
+            icd = [char_freq[0]]
+            icd.append(sum(char_freq[1:4]))
+            icd.append(sum(char_freq[4:7]))
+            icd.append(sum(char_freq[7:12]))
+            icd.append(sum(char_freq[12:16]))
+            icd.append(sum(char_freq[16:256]))
+
+            #Computing x^2 value
+            x2_value = chisquare(icd, [self.attribute_models_["icd"][i]*
+                                       len(row.request_url) for i in range(6)])
+            tmp_lst.append(x2_value.pvalue)
+        anomalous = pd.DataFrame(index=X.index, data=tmp_lst, columns=["pvalue"])
+        result["pvalue"] = anomalous.copy()
+
+        tmp_lst = []
+        for index, row in X.iterrows():
+            params = urlparse.parse_qs(urlparse.urlsplit(row.request_url).query)
+            detected = False
+            if len(params) > 0:
+                keys_set = set(params.keys())
+                if keys_set not in self.attribute_models_["param_sets"]:
+                    detected = True
+            tmp_lst.append(detected)
+        anomalous = pd.DataFrame(index=X.index, data=tmp_lst, columns=["param_sets"])
+        result["param_sets"] = anomalous.copy()
+
+        return result
