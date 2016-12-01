de658b91851481b0d326d62bbac2f06ff5dc1789
cristianorolim <cristianorolim@gmail.com>
2016-12-01 20:09:32 -0200

    Revert "minor bug"
    This reverts commit 095aba8d31b202dcdfa9934faa7be91f343d2b82.

--- teste.py
+++
@@ -1,7 +0,0 @@
-import logging
-from skinfosec.datasets import chuvakin_httpd
-
-logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
-
-X = chuvakin_httpd.fetch_chuvakin_logs()
-print X.head()
